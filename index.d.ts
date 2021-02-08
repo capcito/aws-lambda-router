b5bcbd50adf1265a0cfd012aa854fe60d03dcbfb
Johan Svanborg <johan.svanborg@capcito.com>
2021-02-08 17:36:48 +0100

    Resetting to not include build files

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+import { ProxyIntegrationConfig, ProxyIntegrationEvent } from './lib/proxyIntegration';
+import { SnsConfig, SnsEvent } from './lib/sns';
+import { SqsConfig, SqsEvent } from './lib/sqs';
+import { S3Config, S3Event } from './lib/s3';
+import { Context } from 'aws-lambda';
+export interface RouteConfig {
+    proxyIntegration?: ProxyIntegrationConfig;
+    sns?: SnsConfig;
+    sqs?: SqsConfig;
+    s3?: S3Config;
+    debug?: boolean;
+}
+export declare type RouterEvent = ProxyIntegrationEvent | SnsEvent | SqsEvent | S3Event;
+export declare const handler: (routeConfig: RouteConfig) => <TContext extends Context>(event: RouterEvent, context: TContext) => Promise<any>;
